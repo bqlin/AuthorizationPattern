5ed600e98591caf3b17f5e9c8a6741698dfedb5a
bqlin <bqlins@163.com>
2018-05-11 10:16:25 +0800

    Initial Commit

---
+++ AuthorizationPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AuthorizationPattern
+//
+//  Created by Bq Lin on 2018/5/11.
+//  Copyright © 2018年 Bq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AuthorizationPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AuthorizationPattern
+//
+//  Created by Bq Lin on 2018/5/11.
+//  Copyright © 2018年 Bq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
